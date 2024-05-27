0017a8b09edfa5dcb095c8c23ba97a62afa4e3e1
KezzyNgotho <keziengotho18@gmail.com>
2024-05-27 08:47:49 +0300

    landing page no assets

--- src/hackfinity_frontend/src/App.jsx
+++ src/hackfinity_frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom"; // Import BrowserRouter
 
@@ -6,23 +5,6 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom
 import LandingPage from "./components/common/LandingPage";
 import NoPage from "./components/common/NoPage";
 import SpinLoading from "./components/utils/SpinLoading";
-=======
-import { useState } from 'react';
-import {canisterId,createActor,idlFactory} from "../../declarations/backend"
-function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    const actor = createActor(canisterId,{})
-
-    actor.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
->>>>>>> origin/HEAD
 
 const App = () => {
   return (
